fcdb52c0cf33b27863677bfe1e885b40fd10cdd2
MuhammadFadhil1024 <61375944+MuhammadFadhil1024@users.noreply.github.com>
2020-10-29 23:54:28 +0700

    Tugas Minggu 6

---
+++ E41190547_Muhammad Fadhil Nurhuda/belajar_php/operator.php
@@ -0,0 +1,35 @@
+<?php 
+$a = 5;
+$b = 2;
+
+//Penjumlahan
+$c = $a + $b;
+echo "$a + $b = $c";
+echo "<hr>";
+
+//penguran
+$c = $a - $b; 
+echo "$a + $b = $c";
+echo "<hr>";
+
+//perkalian
+$c = $a * $b; 
+echo "$a * $b = $c";
+echo "<hr>";
+
+//pembagian
+$c = $a / $b; 
+echo "$a / $b = $c";
+echo "<hr>";
+
+//sisa bagi
+$c = $a % $b; 
+echo "$a % $b = $c";
+echo "<hr>";
+
+//pangkat
+$c = $a ** $b; 
+echo "$a ** $b = $c";
+echo "<hr>";
+
+?>
---
+++ E41190547_Muhammad Fadhil Nurhuda/belajar_php/sintax_html_php.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "Belajar php" ?></title>
+</head>
+<body>
+    <?php 
+        echo "saya sedang belajar php";
+        echo "<p>Belajar php hingga menjadi master</p>";
+    ?>
+</body>
+</html>
