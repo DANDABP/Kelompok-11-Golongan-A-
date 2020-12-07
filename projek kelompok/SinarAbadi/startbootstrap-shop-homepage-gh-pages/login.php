bc3f46e903f4514663f608db37297290daa385e6
MuhammadFadhil1024 <61375944+MuhammadFadhil1024@users.noreply.github.com>
2020-12-08 00:04:08 +0700

    from_Login

---
+++ projek kelompok/SinarAbadi/startbootstrap-shop-homepage-gh-pages/login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="icon" type="image/png" href="logo 2.jpg">
+    <title>Sinar Abadi Electronic</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Style Bootstrap -->
+    <link href="css/login.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <form>
+        <div class="container mt-5 p-5 border border-dark rounded">
+            <div class="card-body ">
+                <div class="d-flex flex-row">
+                    <div class="col-lg-5 p-5">
+                        <h2>Login</h2>
+                        <div class="form-group mt-5 mx-4">
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="username">
+                        </div>
+                        <div class="form-group mb-3 mx-4">
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-primary center-block">Submit</button>
+                    </div>
+                    <div class="col">
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </form>
+    <style>
+        .col {
+            background-image: url(onlineshooping.png);
+            background-size: cover;
+        }
+
+        h2 {
+            text-align: center;
+        }
+
+        button {
+            margin-left: 120px;
+        }
+    </style>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
