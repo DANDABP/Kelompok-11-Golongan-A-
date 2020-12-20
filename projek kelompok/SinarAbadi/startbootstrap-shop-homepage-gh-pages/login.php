851d69e815ded9a5cd7ee2404736342a75b1d19b
MuhammadFadhil1024 <61375944+MuhammadFadhil1024@users.noreply.github.com>
2020-12-20 19:37:31 +0700

    update login
     login dengan input email

--- projek kelompok/SinarAbadi/startbootstrap-shop-homepage-gh-pages/login.php
+++ projek kelompok/SinarAbadi/startbootstrap-shop-homepage-gh-pages/login.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 if (isset($_SESSION['username'])) {
-    header("location:admin.php");
-    exit;
+  header("location:admin.php");
+  exit;
 }
 ?>
 
@@ -20,44 +20,44 @@ if (isset($_SESSION['username'])) {
 </head>
 
 <body>
-    <div id="card">
-        <?php
-        if (isset($_GET['pesan'])) {
-          $pesan = $_GET['pesan'];
-          if ($pesan == "gagal") {
-        ?>
-            <div class="alert alert-danger">Anda gagal login silahkan masukkan usernamae dan password dengan benar</div>
-        <?php
-          }
-        }
-        ?>
-        <div id="card-content">
-          <div id="card-title">
-            <h2>LOGIN</h2>
-            <div class="underline-title"></div>
-          </div>
-          <form method="post" class="form">
+  <div id="card">
+    <?php
+    if (isset($_GET['pesan'])) {
+      $pesan = $_GET['pesan'];
+      if ($pesan == "gagal") {
+    ?>
+        <div class="alert alert-danger">Anda gagal login silahkan masukkan usernamae dan password dengan benar</div>
+    <?php
+      }
+    }
+    ?>
+    <div id="card-content">
+      <div id="card-title">
+        <h2>LOGIN</h2>
+        <div class="underline-title"></div>
+      </div>
+      <form method="post" class="form" action="login-proses.php">
 
-            <label for="user-email" style="padding-top:13px">
-                &nbsp;Email
-            </label>
-            <input id="user-email" class="form-content" type="email" name="email" autocomplete="on" required />
-            <div class="form-border"></div>
+        <label for="user-email" style="padding-top:13px">
+          &nbsp;Email
+        </label>
+        <input id="email" class="form-content" type="email" name="email" autocomplete="on" required />
+        <div class="form-border"></div>
 
-            <label for="user-password" style="padding-top:22px">
-                &nbsp;Password
-            </label>
-            <input id="user-password" class="form-content" type="password" name="password" required />
-            <div class="form-border"></div>
-        
-            <a href="#">
-              <legend id="forgot-pass">Forgot password?</legend>
-            </a>
-        
-            <input id="submit-btn" type="submit" name="submit" value="LOGIN" />
+        <label for="user-password" style="padding-top:22px">
+          &nbsp;Password
+        </label>
+        <input id="password" class="form-content" type="password" name="password" required />
+        <div class="form-border"></div>
 
-          </form>
-        </div>
+        <a href="#">
+          <legend id="forgot-pass">Forgot password?</legend>
+        </a>
+
+        <input id="submit-btn" type="submit" name="submit" value="LOGIN" />
+
+      </form>
+    </div>
   </div>
 </body>
 
